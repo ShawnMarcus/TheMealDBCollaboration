722e8f2bdd9d487e316590d93ba9197dfc658d2f
Shawn Marcus <smarcus@post.com>
2021-11-14 20:45:02 -0500

    Created the get_meal_btn and meal_container const variables. Added the addEventListener to fetch a random meal from the API when the get_meal_btn is clicked.

---
+++ app.js
@@ -0,0 +1,12 @@
+// https://www.themealdb.com/api/json/v1/1/random.php.
+
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
+
+get_meal_btn.addEventListener('click', () => {
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(res => {
+    createMeal(res.meals[0]);
+  });
+});
